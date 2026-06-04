fc29c0488f47b3743fe4febb07e409b3d8c5b901
ryuuta <ryuuta.k0926@gmail.com>
2026-06-04 11:01:06 +0900

    要らないファイルを削除

--- 課題用ファイル/keisanzyouhougaku4/最終課題/草案２_乱高下版.r
+++ 課題用ファイル/keisanzyouhougaku4/最終課題/草案２_乱高下版.r
@@ -200,7 +200,7 @@ plot(history_S,
 )
 lines(history_M, col = "green")
 lines(history_B, col = "red")
-legend("topleft", legend = c("株価 (S)", "マネー (M)", "債券 (B)"), col = c("blue", "green", "red"), lty = 1)
+legend("topleft", legend = c("株価 (S)", "マネー (M)", "安定資産 (B)"), col = c("blue", "green", "red"), lty = 1)
 
 # グラフ2: 政策金利の推移
 plot(history_R,
