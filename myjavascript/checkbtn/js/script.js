435302a846cf48e2f0c9117d2a08aa30169a35aa
ryuuta <ryuuta.k0926@gmail.com>
2026-07-01 02:09:39 +0900

    やっと送信ボタンが作れた

--- myjavascript/checkbtn/js/script.js
+++ myjavascript/checkbtn/js/script.js
@@ -4,3 +4,11 @@ const btn = document.getElementById("btn");
 isAgreed.addEventListener("change", () => {
     btn.disabled = !isAgreed.checked;
 });
+
+const sousin = () => {
+    alert('送信します')
+}
+
+btn.addEventListener('click', sousin)
+
+
