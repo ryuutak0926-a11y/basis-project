fc812633408e13cf386d8d598eca574fb0704f88
ryuuta <ryuuta.k0926@gmail.com>
2026-07-09 19:36:33 +0900

    menutable制作途中 5-2中間

---
+++ myjavascript/menutable/js/script.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector("#menu");
+
+const content = `<div><img src="images/cat_dog.jpg" alt=""></div>`
+//menu.textContent = content;
+//上のじゃ文字列が出来るだけでうまくいかない
---
+++ myjavascript/pagescroll/js/script.js
@@ -0,0 +1,11 @@
+const getScrollPercent = () => {
+    const scrolled = window.scrollY;
+    console.log(`${scrolled}ピクセルスクロールしました。`)
+    const pageHeight = document.documentElement.scrollHeight;
+    const viewHeight = document.documentElement.clientHeight;
+    const percentage = scrolled / (pageHeight - viewHeight) * 100;
+    document.querySelector(`#bar`).style.width = `${percentage}%`;
+}
+
+window.addEventListener(`scroll`, getScrollPercent);
+
