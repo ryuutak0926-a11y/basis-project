19213fbcb8a2c354d58a873fc891759677d38532
ryuuta <ryuuta.k0926@gmail.com>
2026-07-11 00:16:16 +0900

    menutable 画像は表示できたが、コメントクリック機能は作れていない

--- myjavascript/menutable/js/script.js
+++ myjavascript/menutable/js/script.js
@@ -1,5 +1,44 @@
 const menu = document.querySelector("#menu");
 
-const content = `<div><img src="images/cat_dog.jpg" alt=""></div>`
+// const sakumatu = {
+//     name: "さくらとまっちゃん",
+//     img:"cat_dog.jpg",
+//     pr:"はなちゅう可愛い"
+// };
+
+const pets = [
+    "../img/cat_dog.jpg",
+    "../img/cat_ma.jpg",
+    "../img/cat_me.jpg",
+    "../img/cat_qu.jpg",
+    "../img/dog_sa.jpg"
+];
+
+const =[
+    { さくらとまっちゃん }
+    { まっちゃん }
+    { めーちゃん }
+    { くーちゃん }
+    { さくらちゃん }
+];
+
+
+
+// const list1 = `<div><img src="../img/cat_dog.jpg" alt=""></div>`
+// const list2 = `<div><img src="../img/cat_ma.img" alt=""></div>`
+// const list3 = `<div><img src="../img/cat_me.img" alt=""></div>`
+// const list4 = `<div><img src="../img/cat_qu.img" alt=""></div>`
+// const list5 = `<div><img src="../img/dog_sa.img" alt=""></div>`
+//これでやると定数を大量に定義しなくてはいけない
+
+
+
 //menu.textContent = content;
 //上のじゃ文字列が出来るだけでうまくいかない
+
+//menu.insertAdjacentHTML("beforeend", pets);
+
+// for (let pet of pets) {
+//     menu.insertAdjacentHTML("beforeend", `<div><img src="${pet}" alt=""></div>`);
+// }
+
