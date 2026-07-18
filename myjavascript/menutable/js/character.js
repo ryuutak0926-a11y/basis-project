a99356915b9e3e7a02d5d92a40eb2576c9c0ff88
ryuuta <ryuuta.k0926@gmail.com>
2026-07-18 20:38:05 +0900

    sakumatu修正中

--- myjavascript/menutable/js/character.js
+++ myjavascript/menutable/js/character.js
@@ -1,8 +1,14 @@
-const hamburger = document.querySelector(`.hamburger`);
-const menu = document.querySelector(".menu");
+const body = document.querySelector('body');
+const btn = document.querySelector('.toggle_btn');
+const mask = document.querySelector('#mask');
+const open = 'open';
 
-//alert("つながり確認");
+// ボタンをクリックしたら、openクラスを「トグル（交互に切り替え）」する
+btn.addEventListener('click', () => {
+    body.classList.toggle(open);
+});
 
-hamburger.addEventListener(`click`, () => {
-    menu.classList.toggle(`active`);
-})
+// マスク（背景）をクリックしたら、openクラスを確実に消す
+mask.addEventListener('click', () => {
+    body.classList.remove(open);
+});
